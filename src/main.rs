1b5c39ea9167561c4a0d1ec4f8bf0253c8f38b46
n8tbyte <naruepanart1201@gmail.com>
2026-03-30 20:11:07 +0700

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,10 @@ slint::include_modules!();
 fn main() -> Result<(), slint::PlatformError> {
     unsafe {
         std::env::set_var("SLINT_BACKEND", "software");
+        std::env::set_var("SLINT_SCALE_FACTOR", "1");
+        std::env::set_var("SLINT_ANIMATIONS", "0");
+        std::env::set_var("SLINT_DEBUG_PERFORMANCE", "0");
+        std::env::set_var("SLINT_DEBUG_REDRAW", "0"); 
     }
 
     let ui = AppWindow::new()?;
